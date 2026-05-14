e896d53ce0b1625c8af9ae826d8fb2b8233968c9
Rossin <rossin@rossinvendas.com>
2026-05-14 16:29:25 -0300

    feat: Update favicon and social media image
    Updates the favicon to the Percom logo and the Open Graph image for social media sharing. Also, corrects the image URL used in the dynamic image fetching script.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import https from 'https';
-https.get('https://ibb.co/gMQQvWDf', (res) => {
+https.get('https://ibb.co/cXhpjzVv', (res) => {
   let data = '';
   res.on('data', (chunk) => data += chunk);
   res.on('end', () => console.log(data.match(/https:\/\/i\.ibb\.co\/[^"']+/g)[0]));
