ce9d354e4d600aa42f315e5eaa8a016802d282a1
Rossin <rossin@rossinvendas.com>
2026-05-14 16:33:14 -0300

    fix: Update favicon and image links
    Replaced the old favicon and an image URL in the script with updated ones to reflect current branding assets.

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 import https from 'https';
-https.get('https://ibb.co/cXhpjzVv', (res) => {
+https.get('https://ibb.co/tMPnv1DW', (res) => {
   let data = '';
   res.on('data', (chunk) => data += chunk);
   res.on('end', () => console.log(data.match(/https:\/\/i\.ibb\.co\/[^"']+/g)[0]));
